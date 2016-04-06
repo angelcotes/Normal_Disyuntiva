6f212ee98be22e1ede8bff4592bc1dcc7aed1612
unknown <angelcotes@gmail.com>
2016-04-06 16:40:38 -0500

    Conditions when the program present problems into data

--- Laboratorio.py
+++ Laboratorio.py
@@ -9,10 +9,16 @@ class DataTable():
     Matriz_data = [ [ 1 for i in range(100) ] for j in range(100) ]
     NumF = 0
     NumC = 0
+    Data = []
     def read_file(self, fileName):
         lines = []        
         with open(fileName, 'r') as f:
             for lines in f:
+                self.formatString = lines
+                if lines in self.Data:
+                    del self.Data[:]
+                else:
+                    self.Data.append(deepcopy(lines))
                 line = lines.strip().split(",")
                 self.NumC = 0
                 for row in line:
@@ -82,107 +88,108 @@ R = []
 N = []
 nP = []
 nN = []
-index = 0
-print '-------'*(objclass.getNumC())
-while (index < objclass.getNumC()):
-    print '  F'+str(index)+' ',
-    index = index + 1
-print '  Y  '
-print '-------'*(index)
-objProcess.PrintMatrix(objclass)
-print '-------'*(index)
-index = 0
-while ( index <= objclass.getNumF()):
-    if (objclass.getData(index, objclass.getNumC()) == 1):
-        P.append(index)
-    index = index + 1
-while (len(P) != 0):
-    if (len(N) == 0):
-        index = 0
-        while (index <= objclass.getNumF()):
-            if (objclass.getData(index,objclass.getNumC()) == 0):
-                N.append(index)
-            index = index + 1
+if len(objclass.Data) > 0:
     index = 0
+    print '-------'*(objclass.getNumC())
+    while (index < objclass.getNumC()):
+        print '  F'+str(index)+' ',
+        index = index + 1
+    print '  Y  '
+    print '-------'*(index)
+    objProcess.PrintMatrix(objclass)
+    print '-------'*(index)
+    index = 0
+    while ( index <= objclass.getNumF()):
+        if (objclass.getData(index, objclass.getNumC()) == 1):
+            P.append(index)
+        index = index + 1
+    while (len(P) != 0):
+        if (len(N) == 0):
+            index = 0
+            while (index <= objclass.getNumF()):
+                if (objclass.getData(index,objclass.getNumC()) == 0):
+                    N.append(index)
+                index = index + 1
+        index = 0
+        print
+        print '-------'*(objclass.getNumC())
+        print 'Vector P: ',P
+        print '-------'*(objclass.getNumC())
+        print 'Vector N: ', N
+        print '-------'*(objclass.getNumC())
+        position = -1
+        divResul = -1
+        while (index <= objclass.getNumC() - 1):
+            nP.append(objProcess.Find_Ns(R, P, objclass, index, 'P'))
+            nN.append(objProcess.Find_Ns(R, N, objclass, index, 'N'))
+            if (position == -1):
+                position = 0
+                divResul = float(nP[index])/ float(nN[index])
+            elif (divResul < float(nP[index])/ float(nN[index])):
+                divResul = float(nP[index])/ float(nN[index])
+                position = index
+            index = index + 1
+        print 'Probando P en Ro: ', nP
+        print '-------'*(objclass.getNumC())
+        print 'Probando N en Ro: ', nN
+        print '-------'*(objclass.getNumC())
+        print 'Funcion Mayor Valor: ', 'F'+str(position)
+        print '-------'*(objclass.getNumC())
+        if position in R:
+            break
+        R.append(position)
+        indexN = 0
+        del nP[:]
+        del nN[:]
+        while (indexN < len(N)):
+            if (objProcess.Result_Explication(R, objclass, N, indexN) == objclass.getData(N[indexN], objclass.getNumC())):
+                N.pop(indexN)
+            else:
+                indexN = indexN + 1
+        if (len(N) == 0):
+            H.append(deepcopy(R))
+            indexH = 0
+            del R[:]
+            while (indexH < len(H)):
+                indexP = 0
+                while (indexP < len(P)):
+                    if (objProcess.Result_Explication(H[indexH], objclass, P, indexP) == objclass.getData(P[indexP], objclass.getNumC())):
+                        P.remove(P[indexP])
+                    else:
+                        indexP = indexP + 1
+                indexH = indexH + 1
     print
     print '-------'*(objclass.getNumC())
-    print 'Vector P: ',P
+    print ' Ecuacion '
     print '-------'*(objclass.getNumC())
-    print 'Vector N: ', N
+    indexF = 0
+    Funtion = ''
+    while (indexF < len(H)):
+        indexC = 0
+        FuntionSub = ''
+        while (indexC < len(H[indexF])):
+            if (len(FuntionSub) == 0):
+                FuntionSub = FuntionSub + 'F' + str(H[indexF][indexC])
+            else:
+                FuntionSub = FuntionSub + ' * F' + str(H[indexF][indexC])
+            indexC = indexC + 1
+        indexF = indexF + 1
+        if (indexF < len(H)):
+            Funtion = Funtion + '( ' + FuntionSub + ' ) + '
+        else:
+            Funtion = Funtion + '( ' + FuntionSub + ' ) '
+    if (len(Funtion) == 0):
+        print 'No tiene solucion',
+    print (Funtion)
     print '-------'*(objclass.getNumC())
-    position = -1
-    divResul = -1
-    while (index <= objclass.getNumC() - 1):
-        nP.append(objProcess.Find_Ns(R, P, objclass, index, 'P'))
-        nN.append(objProcess.Find_Ns(R, N, objclass, index, 'N'))
-        if (position == -1):
-            position = 0
-            divResul = float(nP[index])/ float(nN[index])
-        elif (divResul < float(nP[index])/ float(nN[index])):
-            divResul = float(nP[index])/ float(nN[index])
-            position = index
+else:
+    index = 0
+    print '-------'*(objclass.getNumC())
+    while (index < objclass.getNumC()):
+        print '  F'+str(index)+' ',
         index = index + 1
-    print 'Probando P en Ro: ', nP
-    print '-------'*(objclass.getNumC())
-    print 'Probando N en Ro: ', nN
-    print '-------'*(objclass.getNumC())
-    print 'Funcion Mayor Valor: ', 'F'+str(position)
-    print '-------'*(objclass.getNumC())
-    if position in R:
-        break
-    R.append(position)
-    indexN = 0
-    del nP[:]
-    del nN[:]
-    while (indexN < len(N)):
-        if (objProcess.Result_Explication(R, objclass, N, indexN) == objclass.getData(N[indexN], objclass.getNumC())):
-            N.pop(indexN)
-        else:
-            indexN = indexN + 1
-    if (len(N) == 0):
-        H.append(deepcopy(R))
-        indexH = 0
-        del R[:]
-        while (indexH < len(H)):
-            indexP = 0
-            while (indexP < len(P)):
-                if (objProcess.Result_Explication(H[indexH], objclass, P, indexP) == objclass.getData(P[indexP], objclass.getNumC())):
-                    P.remove(P[indexP])
-                else:
-                    indexP = indexP + 1
-            indexH = indexH + 1
-print
-print '-------'*(objclass.getNumC())
-print ' Ecuacion '
-print '-------'*(objclass.getNumC())
-indexF = 0
-Funtion = ''
-while (indexF < len(H)):
-    indexC = 0
-    FuntionSub = ''
-    while (indexC < len(H[indexF])):
-        if (len(FuntionSub) == 0):
-            FuntionSub = FuntionSub + 'F' + str(H[indexF][indexC])
-        else:
-            FuntionSub = FuntionSub + ' * F' + str(H[indexF][indexC])
-        indexC = indexC + 1
-    indexF = indexF + 1
-    if (indexF < len(H)):
-        Funtion = Funtion + '( ' + FuntionSub + ' ) + '
-    else:
-        Funtion = Funtion + '( ' + FuntionSub + ' ) '
-if (len(Funtion) == 0):
-    print 'No tiene solucion',
-print (Funtion)
-print '-------'*(objclass.getNumC())        
-
-
-
-
-
-
-
-
-
-
-
+    print '  Y  '
+    print '-------'*(index)
+    objProcess.PrintMatrix(objclass)
+    print '-------'*(index)
+    print 'No tiene solucion'
